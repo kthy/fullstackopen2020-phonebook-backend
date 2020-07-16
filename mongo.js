c29b2ce7c3de5d8f4345f6b0c0f85ae8ea7336e3
Kristian Thy <thy@42.dk>
2020-07-16 10:57:34 +0200

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const pwd = process.argv[2]
+const url = `mongodb+srv://fullstack:${pwd}@fullstackopen2020.ixhm8.mongodb.net/`
+  + 'phonebook?retryWrites=true&w=majority'
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  person.save().then(_ => {
+    console.log(`${person.name} saved to db`)
+    mongoose.connection.close()
+  })
+}
